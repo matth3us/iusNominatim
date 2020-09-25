5b96b9115d1b89668873067f96c0181a8fa1baae
celso.mattheus <celso.mattheus@gmail.com>
2020-09-25 02:25:47 -0300

    inserido mais visualizacoes na demonstracao

--- demo.r
+++ demo.r
@@ -4,7 +4,7 @@ source('./script/functions.r')
 # Exemplo
 bad_geo <- get_coords_osm("Quatre vingt douze R. de l'Église")
 process_address <- get_libpost_address("Quatre vingt douze R. de l'Église")
-address_processed <- get_better_address("Quatre vingt douze R. de l'Église")
+# address_processed <- get_better_address("Quatre vingt douze R. de l'Église")
 good_geo <- get_coords(process_address)
 
 
@@ -14,13 +14,23 @@ library(sf)
 library(tmap)
 tmap_mode("view")
 
-point <- 
-  sf::st_as_sf(
+
+p1 <- sf::st_as_sf(
     data.frame(lat = good_geo[[1]]$lat, lon = good_geo[[1]]$lon)
     , coords = c("lon", "lat")
     , crs = "WGS84"
   )
 
-tm_shape(point) +
+tm_shape(p1) +
+  tm_dots() +
+  tm_basemap(server = "OpenTopoMap")
+
+p5 <- sf::st_as_sf(
+  data.frame(lat = good_geo[[5]]$lat, lon = good_geo[[5]]$lon)
+  , coords = c("lon", "lat")
+  , crs = "WGS84"
+)
+
+tm_shape(p5) +
   tm_dots() +
   tm_basemap(server = "OpenTopoMap")
